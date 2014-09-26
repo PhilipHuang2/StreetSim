6827cecbd4bf0b995b78627e34127ce6b4f52523
PhilipHuang2 <philiphuang2@gmail.com>
2014-09-26 10:41:24 -0700

    added basic code

--- StreetSim.pde
+++ StreetSim.pde
@@ -1,9 +1,18 @@
 void setup()
 {
-	
+	size(500,500);
+	background(255);
 }
 void draw()
 {
+	//make 50 & 40 a variable
+ 	stroke(255, 236, 94);
+ 	for (int b = 240; b < 261; b = b + 20) {
+ 		for (int a = 0; a < 500; a = a+50) {
+ 			line(a,b,a+40,b);
+ 		}
+ 	}
+
 
 }
 class vehicle
--- build-tmp/source/StreetSim.java
+++ build-tmp/source/StreetSim.java
@@ -15,14 +15,29 @@ import java.io.IOException;
 public class StreetSim extends PApplet {
 
 public void setup()
-{}
+{
+	size(500,500);
+	background(255);
+}
 public void draw()
-{}
+{
+ 	stroke(255, 236, 94);
+ 	for (int b = 240; b < 261; b = b + 20) {
+ 		for (int a = 0; a < 500; a = a+50) {
+ 			line(a,b,a+40,b);
+ 		}
+ 	}
+
+
+}
 class vehicle
 {
 	vehicle()
-	{}
+	{
+
+	}
 }
+
 class person
 {
 	person()
